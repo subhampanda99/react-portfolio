76e11a4b0ef19b2f77d213329b89848a740739e3
Subham Panda <subhampanda2899@gmail.com>
2020-10-28 13:21:07 +0530

    Latest Update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,12 +6,16 @@ class Resume extends Component {
     if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function (education) {
-        return <div key={education.school}><h3>{education.university_1}</h3>
-          <h5>{education.school_1}</h5>
+        return <div key={education.school_1}><h3>{education.school_1}</h3>
           <p className="info">{education.degree_1} <span>&bull;</span><em className="date">{education.graduated_1}</em></p>
-          <p>{education.description}</p></div>
+          <p>{education.description_1}</p></div>
       })
-
+      var work = this.props.data.work.map(function (work) {
+        return <div key={work.company}><h3>{work.company}</h3>
+          <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+          <p>{work.description}</p>
+        </div>
+      })
       var skills = this.props.data.skills.map(function (skills) {
         var className = 'bar-expand ' + skills.name.toLowerCase();
         return <li key={skills.name}><span style={{ width: skills.level }} className={className}></span><em>{skills.name}</em></li>
@@ -36,7 +40,16 @@ class Resume extends Component {
         </div>
 
 
+        <div className="row work">
 
+          <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {work}
+          </div>
+        </div>
 
 
 
